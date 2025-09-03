81481132a1b27343e71b730748a91418123d4e1b
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-03 23:21:03 +0100

    python-generators-0x00/1-batch_processing.py uodated

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -27,13 +27,17 @@ def stream_users_in_batches(batch_size):
 
     cursor.close()
     conn.close()
+    return  # explicitly added return
 
 
 def batch_processing(batch_size):
     """
     Processes batches of users, filtering users over age 25
     """
+    results = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
                 print(user)
+                results.append(user)
+    return results  # explicitly added return
