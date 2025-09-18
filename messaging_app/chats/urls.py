d18d7de747824c627e077d9014036fbb375d79dd
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-18 14:23:22 +0100

    messaging_app/chats/urls.py updated

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-# Use DRF's router to auto-generate routes for ViewSets
-router = routers.DefaultRouter()
+# Use DRF's DefaultRouter to auto-generate routes for ViewSets
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),  # Expose all routes under /api/
 ]
