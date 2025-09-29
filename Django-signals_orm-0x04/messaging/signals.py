6867dcfeda2807e53196052f50a2d79bc385a232
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-29 10:44:46 +0100

    messaging updated

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Message, Notification
+
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    """Automatically create a notification when a new message is sent"""
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class SignalTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username="alice", password="password123")
+        self.receiver = User.objects.create_user(username="bob", password="password123")
+
+    def test_notification_created_on_message(self):
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello Bob!")
+        notification = Notification.objects.get(user=self.receiver, message=message)
+        self.assertEqual(notification.user, self.receiver)
+        self.assertFalse(notification.is_read)
