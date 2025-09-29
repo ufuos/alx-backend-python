1c76614a112ed6a7e981f140c4e1f2c2573082e9
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-29 15:30:29 +0100

    Django-signals_orm-0x04/django_chat/settings.py updated

--- Django-signals_orm-0x04/django_chat/settings.py
+++ Django-signals_orm-0x04/django_chat/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_chat',
+    'rest_framework'
+    
 ]
 
 MIDDLEWARE = [
