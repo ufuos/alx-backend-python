b8e79c2ad4175f49f0e4f33e5f2778b04044a26d
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-10-10 01:25:16 +0100

    messaging_app/.gitignore,messaging_app/docker-compose.yml added and messaging_app/messaging_app/settings.py updated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -101,10 +101,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST', 'db'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
 }
 
 
